9871276437f4a44fe5d99e76d4e325d058b6fac8
David Andrés Garza Medina <davidazullo@gmail.com>
2017-03-09 22:47:54 -0600

    Tarea Bota Pelota
    Movimiento de pelota acabado.

--- Bota Pelota/Lienzo.java
+++ Bota Pelota/Lienzo.java
@@ -3,6 +3,7 @@ import javax.swing.Timer;
 import java.awt.Graphics;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class Lienzo extends JPanel {
     private Pelota pelota;
@@ -10,19 +11,23 @@ public class Lienzo extends JPanel {
     public Lienzo() {
         pelota = new Pelota(100, 100, 10);
         EscucharTiempo escuchaTiempo = new EscucharTiempo();
-        Timer tiempo = new Timer(1000, escuchaTiempo);
+        Timer tiempo = new Timer(50, escuchaTiempo);
         tiempo.start();
     }
     
     class EscucharTiempo implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println("Hola Pelota");
+            pelota.muevete(getBounds());
+            repaint();
         }
     }
     
     @Override
     public void paintComponent(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setColor(Color.BLUE);
         pelota.dibuja(g);
     }
 }
--- Bota Pelota/Pelota.java
+++ Bota Pelota/Pelota.java
@@ -1,17 +1,31 @@
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public class Pelota {
     private int posx;
     private int posy;
+    private int incx;
+    private int incy;
     private int radio;
     
     public Pelota(int x, int y, int r) {
         this.posx = x;
         this.posy = y;
         this.radio = r;
+        incx = 5;
+        incy = 5;
     }
     
     public void dibuja(Graphics g) {
         g.drawOval(posx, posy, radio*2, radio*2);
+    }
+    
+    public void muevete(Rectangle r) {
+        posx += incx;
+        posy += incy;
+        if(posx<=radio||posx+radio>=r.getWidth())
+            incx *= -1;
+        if(posy<=radio||posy+radio>=r.getHeight())
+            incy *= -1;
     }
 }
